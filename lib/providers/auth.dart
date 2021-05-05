1327e404e3c58b3e699aff1ced8c40daef1de413
Dawit Getachew <dgetachew@paga.com>
2021-05-05 14:12:47 +0300

    update authentication classes

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -3,24 +3,43 @@ import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/http_exception.dart';
+
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signUp(String email, String password) async {
-    const url = 'identitytoolkit.googleapis.com';
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url = 'identitytoolkit.googleapis.com';
 
-    final response = await http.post(
-        Uri.https(url, '/v1/accounts:signUp',
+    try {
+      final response = await http.post(
+        Uri.https(url, '/v1/accounts:$urlSegment',
             {'key': 'AIzaSyBk_GxSQZpbELcbqijFD6sS5EveGCHZo7c'}),
-        body: json.encode({
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        }));
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
 
-    print(response.request.url);
-    print(response.body);
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
